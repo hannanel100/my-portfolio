0c3b5c0d0e85f012d82faccd9010854f16b61007
hannanel100 <hannanel100@gmail.com>
2023-04-18 10:43:07 +0300

    added initial sanity schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "sanity";  
-import {deskTool} from "sanity/desk"
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import schemas from "./sanity/schemas";
 const config = defineConfig({
-    projectId: "akedaqmq",
-    dataset: "production",
-    title: "My Portfolio",
-    apiVersion: "2023-17-04",
-    basePath: "/admin",
-    plugins: [deskTool()]
-})
-
+  projectId: "akedaqmq",
+  dataset: "production",
+  title: "My Portfolio",
+  apiVersion: "2023-17-04",
+  basePath: "/admin",
+  plugins: [deskTool()],
+  schema: { types: schemas },
+});
 
 export default config;
