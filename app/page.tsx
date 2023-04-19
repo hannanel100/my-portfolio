9e62671442242dc3642c159bfcf79c8422087d70
hannanel100 <hannanel100@gmail.com>
2023-04-19 16:54:37 +0300

    finished technologies

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,35 @@ import {
   FaPython,
 } from "react-icons/fa";
 import { TbBrandNextjs } from "react-icons/tb";
-import { SiTailwindcss, SiPostgresql, SiTypescript, SiMongodb, SiDocker } from "react-icons/si";
+import {
+  SiTailwindcss,
+  SiPostgresql,
+  SiTypescript,
+  SiMongodb,
+  SiDocker,
+  SiJavascript,
+  SiCss3,
+  SiHtml5,
+  SiFlask,
+  SiExpress,
+  SiPrisma,
+} from "react-icons/si";
 import { Technologies } from "@/types/technologies";
+// add icon to Technologies object
+interface TechnologyProps extends Technologies {
+  icon: JSX.Element;
+}
+const Technology = ({ technology }: { technology: TechnologyProps }) => {
+  return (
+    <div className="flex w-16 flex-col items-center gap-2" key={technology._id}>
+      <span className="grid h-16 place-items-center rounded-lg bg-teal-950 p-2  text-teal-100 sm:w-full">
+        {technology.icon}
+      </span>
+      <span>{technology.title}</span>
+    </div>
+  );
+};
+
 type ButtonProps = {
   children: React.ReactNode;
   href: string;
@@ -56,6 +83,18 @@ const addIconToTechnology = (technology: Technologies) => {
         <SiMongodb className="text-4xl text-orange-300" />
       ) : technology.name === "Docker" ? (
         <SiDocker className="text-4xl text-orange-300" />
+      ) : technology.name === "Javascript" ? (
+        <SiJavascript className="text-4xl text-orange-300" />
+      ) : technology.name === "CSS3" ? (
+        <SiCss3 className="text-4xl text-orange-300" />
+      ) : technology.name === "HTML5" ? (
+        <SiHtml5 className="text-4xl text-orange-300" />
+      ) : technology.name === "Flask" ? (
+        <SiFlask className="text-4xl text-orange-300" />
+      ) : technology.name === "Express" ? (
+        <SiExpress className="text-4xl text-orange-300" />
+      ) : technology.name === "Prisma" ? (
+        <SiPrisma className="text-4xl text-orange-300" />
       ) : (
         <TbBrandNextjs className="text-4xl text-orange-300" />
       ),
@@ -193,46 +232,30 @@ export default async function Home() {
           Technologies
         </h2>
         <div className="mt-5 grid gap-8 sm:grid-cols-2 md:grid-cols-3">
-          <article className="grid grid-rows-[50px_minmax(100px,_1fr)_100px]  justify-between rounded-lg border border-teal-500 border-opacity-40 p-4">
+          <article className="grid grid-rows-[50px_minmax(100px,_1fr)]  justify-between rounded-lg border border-teal-500 border-opacity-40 p-4">
             <div className="bg-gradient-to-r from-teal-500 to-orange-200 bg-clip-text font-extrabold text-transparent">
               Frontend
             </div>
-            <div className="flex flex-wrap items-center justify-between gap-2 ">
+            <div className="flex flex-wrap items-center justify-evenly  gap-8 ">
               {frontend &&
                 frontend.map(
                   (item) =>
                     item?.icon && (
-                      <div
-                        className="flex w-16 flex-col items-center gap-2"
-                        key={item._id}
-                      >
-                        <span className="grid h-16 place-items-center rounded-lg bg-teal-950 p-2  text-teal-100 sm:w-full">
-                          {item.icon}
-                        </span>
-                        <span>{item.title}</span>
-                      </div>
+                      <Technology technology={item} key={item._id} />
                     )
                 )}
             </div>
           </article>
-          <article className="grid grid-rows-[50px_minmax(100px,_1fr)_100px]  justify-between rounded-lg border border-teal-500 border-opacity-40 p-4">
+          <article className="grid grid-rows-[50px_minmax(100px,_1fr)]  justify-between rounded-lg border border-teal-500 border-opacity-40 p-4">
             <div className="bg-gradient-to-r from-teal-500 to-orange-200 bg-clip-text font-extrabold text-transparent">
               Backend
             </div>
-            <div className="flex flex-wrap items-center justify-between gap-2 ">
+            <div className="flex flex-wrap items-center justify-evenly gap-8 ">
               {backend &&
                 backend.map(
                   (item) =>
                     item?.icon && (
-                      <div
-                        className="flex w-16 flex-col items-center gap-2"
-                        key={item._id}
-                      >
-                        <span className="grid h-16 place-items-center rounded-lg bg-teal-950 p-2  text-teal-100 sm:w-full">
-                          {item.icon}
-                        </span>
-                        <span>{item.title}</span>
-                      </div>
+                      <Technology technology={item} key={item._id} />
                     )
                 )}
             </div>
